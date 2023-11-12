745fb7ac98cac1444d52903aa6ddd45d2f799922
HyunggiPark <phg9587@naver.com>
2023-11-12 20:27:32 +0900

    2. create test case registerProduct

--- src/test/java/com/ejoongseok/wmslive/product/feature/RegisterProductTest.java
+++ src/test/java/com/ejoongseok/wmslive/product/feature/RegisterProductTest.java
@@ -1,4 +1,11 @@
 package com.ejoongseok.wmslive.product.feature;
 
-public class RegisterProductTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RegisterProductTest {
+    @Test
+    @DisplayName("상품을 등록한다.")
+    void registerProduct() {
+    }
 }
