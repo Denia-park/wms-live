4a9984f3a3231d24deb97f06d64bce7ea527067f
HyunggiPark <phg9587@naver.com>
2023-11-13 21:42:08 +0900

    12. product JPA Entity로 변환

--- src/main/java/com/ejoongseok/wmslive/product/domain/ProductRepository.java
+++ src/main/java/com/ejoongseok/wmslive/product/domain/ProductRepository.java
@@ -1,23 +1,8 @@
 package com.ejoongseok.wmslive.product.domain;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 @Repository
-public class ProductRepository {
-    private final Map<Long, Product> products = new HashMap<>();
-    private Long nextId = 1L;
-
-    public void save(final Product product) {
-        product.assignId(nextId++);
-        products.put(product.getId(), product);
-    }
-
-    public List<Product> findAll() {
-        return new ArrayList<>(products.values());
-    }
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
