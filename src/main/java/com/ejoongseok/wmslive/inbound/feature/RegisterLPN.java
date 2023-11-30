438ad72870a69abd72eef41fc86219b09b330008
HyunggiPark <phg9587@naver.com>
2023-12-01 02:23:23 +0900

    LPN 등록 Rest Controller 변경

--- src/main/java/com/ejoongseok/wmslive/inbound/feature/RegisterLPN.java
+++ src/main/java/com/ejoongseok/wmslive/inbound/feature/RegisterLPN.java
@@ -2,36 +2,52 @@ package com.ejoongseok.wmslive.inbound.feature;
 
 import com.ejoongseok.wmslive.inbound.domain.Inbound;
 import com.ejoongseok.wmslive.inbound.domain.InboundRepository;
-import org.springframework.stereotype.Component;
+import com.ejoongseok.wmslive.inbound.domain.LPNRepository;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
-@Component
+@RestController
 class RegisterLPN {
     private final InboundRepository inboundRepository;
+    private final LPNRepository lPNRepository;
 
-    RegisterLPN(final InboundRepository inboundRepository) {
+    RegisterLPN(final InboundRepository inboundRepository,
+                final LPNRepository lPNRepository) {
         this.inboundRepository = inboundRepository;
+        this.lPNRepository = lPNRepository;
     }
 
     @Transactional
-    public void request(final Request request) {
-        final Inbound inbound = inboundRepository.getByInboundItemNo(request.inboundItemNo);
+    @PostMapping("/inbounds/inbound-items/{inboundItemNo}/lpns")
+    public void request(@PathVariable final Long inboundItemNo,
+                        @RequestBody @Valid final Request request) {
+        checkIfLPNBarcodeAlreadyExists(request.lpnBarcode);
 
-        inbound.registerLPN(request.inboundItemNo, request.lpnBarcode, request.expirationAt);
+        final Inbound inbound = inboundRepository.getByInboundItemNo(inboundItemNo);
+
+        inbound.registerLPN(inboundItemNo, request.lpnBarcode, request.expirationAt);
+    }
+
+    private void checkIfLPNBarcodeAlreadyExists(final String lpnBarcode) {
+        lPNRepository.findByLpnBarcode(lpnBarcode)
+                .ifPresent(lpn -> {
+                    throw new LPNBarcodeAlreadyExistException(lpnBarcode);
+                });
     }
 
     public record Request(
-            Long inboundItemNo,
+            @NotBlank(message = "LPN 바코드는 필수입니다.")
             String lpnBarcode,
+            @NotNull(message = "유통기한은 필수입니다.")
             LocalDateTime expirationAt
     ) {
-        public Request {
-            Assert.notNull(inboundItemNo, "LPN을 등록할 입고 상품 번호는 필수입니다.");
-            Assert.hasText(lpnBarcode, "LPN 바코드는 필수입니다.");
-            Assert.notNull(expirationAt, "유통기한은 필수입니다.");
-        }
     }
 }
