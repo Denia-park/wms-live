041290eabc30b9faccf751023ef47ad79023426d
HyunggiPark <phg9587@naver.com>
2023-12-05 00:34:07 +0900

    19. create fields LocationLPN

--- src/main/java/com/ejoongseok/wmslive/location/domain/LocationLPN.java
+++ src/main/java/com/ejoongseok/wmslive/location/domain/LocationLPN.java
@@ -1,4 +1,23 @@
 package com.ejoongseok.wmslive.location.domain;
 
+import com.ejoongseok.wmslive.inbound.domain.LPN;
+
 public class LocationLPN {
+    private final Location location;
+    private final LPN lpn;
+    private Long inventoryQuantity;
+
+    public LocationLPN(final Location location, final LPN lpn) {
+        this.location = location;
+        this.lpn = lpn;
+        inventoryQuantity = 1L;
+    }
+
+    public void increaseQuantity() {
+        inventoryQuantity++;
+    }
+
+    public LPN getLpn() {
+        return this.lpn;
+    }
 }
