b2ee621704de53c77947c0e889d39e19228df610
HyunggiPark <phg9587@naver.com>
2023-12-04 15:09:45 +0900

    2. create test case RegisterLocationTest

--- src/test/java/com/ejoongseok/wmslive/location/feature/RegisterLocationTest.java
+++ src/test/java/com/ejoongseok/wmslive/location/feature/RegisterLocationTest.java
@@ -1,4 +1,21 @@
 package com.ejoongseok.wmslive.location.feature;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RegisterLocationTest {
+    @Test
+    @DisplayName("로케이션을 등록한다")
+    void registerLocation() {
+        //given
+
+
+        //when
+
+
+        //then
+        assertThat(19).isEqualTo();
+    }
 }
