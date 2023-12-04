07b9390feb3a039f924b4dec2786caa6a4da0963
Hyunggi <80137359+Denia-park@users.noreply.github.com>
2023-12-04 23:00:42 +0900

    WMS-6 | 로케이션 등록 API 추가 (#5)
    * 1. create RegisterLocationTest
    
    * 2. create test case RegisterLocationTest
    
    * 3. create inner class RegisterLocation
    
    * 4. create method : request
    
    * 5. create request inner record
    
    * 6. change signature request
    
    * 7. create inner class Location
    
    * 8. add validate
    
    * 9. create LocationRepository(HashMap)
    
    * 10. (test pass) register location
    
    * 11. move inner class to upper level
    
    * 12. change test case -> API Test
    
    * 13. create class RegisterLocationApi
    
    * 14. RegisterLocationApi 구현
    
    * 15. change request validation: springframework.Assert -> jakarta.validation
    
    * 16. location domain JPA 엔티티 적용

---
+++ src/main/java/com/ejoongseok/wmslive/location/domain/StorageType.java
@@ -0,0 +1,11 @@
+package com.ejoongseok.wmslive.location.domain;
+
+public enum StorageType {
+    TOTE("토트 바구니");
+
+    private final String description;
+
+    StorageType(final String description) {
+        this.description = description;
+    }
+}
