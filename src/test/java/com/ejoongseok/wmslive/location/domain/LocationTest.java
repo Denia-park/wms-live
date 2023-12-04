d715396a8c5463fcb3a7c31de28423c686cc1291
HyunggiPark <phg9587@naver.com>
2023-12-05 00:05:54 +0900

    15. extract method

--- src/test/java/com/ejoongseok/wmslive/location/domain/LocationTest.java
+++ src/test/java/com/ejoongseok/wmslive/location/domain/LocationTest.java
@@ -12,15 +12,25 @@ class LocationTest {
     @Test
     @DisplayName("로케이션에 LPN을 할당한다.")
     void assignLPN() {
+        final Location location = createLocation();
+
+        final LPN lpn = createLPN();
+
+        location.assignLPN(lpn);
+    }
+
+    private LPN createLPN() {
+        final LocalDateTime expirationAt = LocalDateTime.now().plusDays(1);
+        final String lpnBarcode = "LPN-1";
+        final LPN lpn = new LPN(lpnBarcode, expirationAt, InboundItemFixture.aInboundItem().build());
+        return lpn;
+    }
+
+    private Location createLocation() {
         final String locationBarcode = "A-1-1";
         final StorageType storageType = StorageType.TOTE;
         final UsagePurpose usagePurpose = UsagePurpose.MOVE;
         final Location location = new Location(locationBarcode, storageType, usagePurpose);
-
-        final LocalDateTime expirationAt = LocalDateTime.now().plusDays(1);
-        final String lpnBarcode = "LPN-1";
-        final LPN lpn = new LPN(lpnBarcode, expirationAt, InboundItemFixture.aInboundItem().build());
-
-        location.assignLPN(lpn);
+        return location;
     }
 }
