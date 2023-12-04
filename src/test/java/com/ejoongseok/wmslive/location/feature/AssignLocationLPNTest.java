76ec47c593d68be660c621921b530cb1d3ec4af7
HyunggiPark <phg9587@naver.com>
2023-12-04 23:37:15 +0900

    3. create inner class AssignLocationLPN

--- src/test/java/com/ejoongseok/wmslive/location/feature/AssignLocationLPNTest.java
+++ src/test/java/com/ejoongseok/wmslive/location/feature/AssignLocationLPNTest.java
@@ -1,12 +1,31 @@
 package com.ejoongseok.wmslive.location.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class AssignLocationLPNTest {
+class AssignLocationLPNTest {
+
+    private AssignLocationLPN assignLocationLPN;
+
+    @BeforeEach
+    void setUp() {
+        assignLocationLPN = new AssignLocationLPN();
+    }
+
     @Test
     @DisplayName("로케이션에 LPN을 할당한다.")
     void assignLocationLPN() {
+        final AssignLocationLPN.Request request = new AssignLocationLPN.Request();
+        assignLocationLPN.assignLocationLPN(request);
+    }
 
+    private class AssignLocationLPN {
+        public void assignLocationLPN(final Request request) {
+
+        }
+
+        public record Request() {
+        }
     }
 }
