e5f5d9f1bb5c6829bdbc0a3bed5cdc2b6663bd62
HyunggiPark <phg9587@naver.com>
2023-12-05 21:15:27 +0900

    6. change signature

--- src/test/java/com/ejoongseok/wmslive/outbound/feature/RegisterPackagingMaterialTest.java
+++ src/test/java/com/ejoongseok/wmslive/outbound/feature/RegisterPackagingMaterialTest.java
@@ -19,7 +19,27 @@ public class RegisterPackagingMaterialTest {
     @DisplayName("포장재를 등록한다.")
     void registerPackagingMaterial() {
         //given
-        final RegisterPackagingMaterial.Request request = new RegisterPackagingMaterial.Request();
+        final Long innerWidthInMillimeters = 1000L;
+        final Long innerHeightInMillimeters = 1000L;
+        final Long innerLengthInMillimeters = 1000L;
+        final Long outerWidthInMillimeters = 1000L;
+        final Long outerHeightInMillimeters = 1000L;
+        final Long outerLengthInMillimeters = 1000L;
+        final Long weightInGrams = 100L;
+        final Long maxWeightInGrams = 10000L;
+        final RegisterPackagingMaterial.Request request = new RegisterPackagingMaterial.Request(
+                "name",
+                "code",
+                innerWidthInMillimeters,
+                innerHeightInMillimeters,
+                innerLengthInMillimeters,
+                outerWidthInMillimeters,
+                outerHeightInMillimeters,
+                outerLengthInMillimeters,
+                weightInGrams,
+                maxWeightInGrams,
+                MaterialType.CORRUGATED_BOX
+        );
         registerPackagingMaterial.request(request);
 
 
@@ -30,12 +50,25 @@ public class RegisterPackagingMaterialTest {
         assertThat(19).isEqualTo();
     }
 
+    private enum MaterialType {
+        CORRUGATED_BOX("골판지 상자");
+
+        private final String description;
+
+        MaterialType(final String description) {
+            this.description = description;
+        }
+    }
+
     private class RegisterPackagingMaterial {
         public void request(final Request request) {
 
         }
 
-        public record Request() {
+        public record Request(String name, String code, long innerWidthInMillimeters, long innerHeightInMillimeters,
+                              long innerLengthInMillimeters, long outerWidthInMillimeters,
+                              long outerHeightInMillimeters, long outerLengthInMillimeters, Long weightInGrams,
+                              Long maxWeightInGrams) {
         }
     }
 }
