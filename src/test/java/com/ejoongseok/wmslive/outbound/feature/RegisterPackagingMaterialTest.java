ac19e9f7d20ed83f2c9256935d2a8a2a4d4a3dc8
HyunggiPark <phg9587@naver.com>
2023-12-05 21:03:57 +0900

    2. create test case

--- src/test/java/com/ejoongseok/wmslive/outbound/feature/RegisterPackagingMaterialTest.java
+++ src/test/java/com/ejoongseok/wmslive/outbound/feature/RegisterPackagingMaterialTest.java
@@ -1,4 +1,21 @@
 package com.ejoongseok.wmslive.outbound.feature;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RegisterPackagingMaterialTest {
+    @Test
+    @DisplayName("포장재를 등록한다.")
+    void registerPackagingMaterial() {
+        //given
+
+
+        //when
+
+
+        //then
+        assertThat(19).isEqualTo();
+    }
 }
