aae2f4c06b5872d4b77bfdb0b487818c19f8390e
HyunggiPark <phg9587@naver.com>
2023-12-05 21:32:46 +0900

    7. add validation

--- src/test/java/com/ejoongseok/wmslive/outbound/feature/RegisterPackagingMaterialTest.java
+++ src/test/java/com/ejoongseok/wmslive/outbound/feature/RegisterPackagingMaterialTest.java
@@ -3,6 +3,7 @@ package com.ejoongseok.wmslive.outbound.feature;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -65,10 +66,47 @@ public class RegisterPackagingMaterialTest {
 
         }
 
-        public record Request(String name, String code, long innerWidthInMillimeters, long innerHeightInMillimeters,
-                              long innerLengthInMillimeters, long outerWidthInMillimeters,
-                              long outerHeightInMillimeters, long outerLengthInMillimeters, Long weightInGrams,
-                              Long maxWeightInGrams) {
+        public record Request(String name, String code, Long innerWidthInMillimeters, Long innerHeightInMillimeters,
+                              Long innerLengthInMillimeters, Long outerWidthInMillimeters,
+                              Long outerHeightInMillimeters, Long outerLengthInMillimeters, Long weightInGrams,
+                              Long maxWeightInGrams, MaterialType corrugatedBox) {
+            public Request {
+                Assert.hasText(name, "포장재 이름은 필수입니다.");
+                Assert.hasText(code, "포장재 코드는 필수입니다.");
+                Assert.notNull(innerWidthInMillimeters, "내부 폭은 필수입니다.");
+                if (innerWidthInMillimeters < 1L) {
+                    throw new IllegalArgumentException("내부 폭은 1mm 이상이어야 합니다.");
+                }
+                Assert.notNull(innerHeightInMillimeters, "내부 높이는 필수입니다");
+                if (innerHeightInMillimeters < 1L) {
+                    throw new IllegalArgumentException("내부 높이는 1mm 이상이어야 합니다.");
+                }
+                Assert.notNull(innerLengthInMillimeters, "내부 길이는 필수입니다");
+                if (innerLengthInMillimeters < 1L) {
+                    throw new IllegalArgumentException("내부 길이는 1mm 이상이어야 합니다.");
+                }
+                Assert.notNull(outerWidthInMillimeters, "외부 폭은 필수입니다.");
+                if (outerWidthInMillimeters < 1L) {
+                    throw new IllegalArgumentException("외부 폭은 1mm 이상이어야 합니다.");
+                }
+                Assert.notNull(outerHeightInMillimeters, "외부 높이는 필수입니다.");
+                if (outerHeightInMillimeters < 1L) {
+                    throw new IllegalArgumentException("외부 높이는 1mm 이상이어야 합니다.");
+                }
+                Assert.notNull(outerLengthInMillimeters, "외부 길이는 필수입니다.");
+                if (outerLengthInMillimeters < 1L) {
+                    throw new IllegalArgumentException("외부 길이는 1mm 이상이어야 합니다.");
+                }
+                Assert.notNull(weightInGrams, "무게는 필수입니다.");
+                if (weightInGrams < 1L) {
+                    throw new IllegalArgumentException("무게는 1g 이상이어야 합니다.");
+                }
+                Assert.notNull(maxWeightInGrams, "최대 무게는 필수입니다.");
+                if (maxWeightInGrams < 1L) {
+                    throw new IllegalArgumentException("최대 무게는 1g 이상이어야 합니다.");
+                }
+                Assert.notNull(corrugatedBox, "포장재 종류는 필수입니다.");
+            }
         }
     }
 }
